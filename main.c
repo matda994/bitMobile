d0f8afe0e4a350f90f63d5f3e35d682771c05f90
oakstreamh <ekstrom.hjalmar@gmail.com>
2017-04-06 15:48:25 +0200

    pls
    pls

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <avr/interrupt.h>
 #define F_CPU 14745600
 
-void pidController(void);
+int pidController(double d_lateral);
 
 double error = 0;
 double error_prior = 0;
@@ -15,36 +15,31 @@ double v = 0;
 double derivative = 0; 
 double dt = 1;	 
 double output = 0;
-double bias = 0; //maybe not include
+static double d_ref = 25.0;
+
+double distance = 0;
+double notDetected = 2;
+double detected = 1; 
+double close = 0.5;
+double cruise;
+double low;
+
+int pidController(double d_lateral)
+{
+		error = d_ref - d_lateral;
+		derivative = (error-error_prior)/dt;
+		output = KP*error + KD*derivative;
+		error_prior = error;
+		
+		return (int) output;	
+}	
+
+
 
 int main(void)
 {
-	//Feedback loop
+	//Feedback loop 
+	
 
-	if (Distance = notDetected)
-	{	
-		v_ref = cruise;
-
-	} 
-	else if (distance = detected) 
-	{
-		v_ref = low;
-	}
-		
-	else if (distance = 	
-	while(1)
-	{
-		//TODO: reset timer
-		//TODO: maybe write code to escape loop on receiving keyboard interrupt?
-		error = v_ref - v; 
-		derivative = (error-error_prior)/dt;
-		output = KP*error + KD*derivative + bias;
-		error_prior = error;
-		
-		//setSomething(output) OR setSomething(converter(output)); //input clock cycles or convert clock cycles to velocity
-		
-		//TODO: "sleep until timer counted to dt" to Synchronize rate
-		
-	}
 	return 1;
 }
