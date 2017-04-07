6ae3a8598d981c0e63c4e08dc6df16c1eddc019e
SimmaLimma <simonmaartensson@gmail.com>
2017-04-07 13:06:22 +0200

    Update uart_buffer.h

--- uart_buffer.h
+++ uart_buffer.h
@@ -1,1 +1,32 @@
 
+/* The best include file in the world */
+
+//NO guarantee that flags work properly. Not tested yet.
+
+/*
+*guide I am using
+*
+*/
+
+#ifndef INCL_GUARD_BUFFER
+#define INCL_GUARD_BUFFER
+
+//Buffer size. To change buffer size, change here. It is now set to 64
+#define BUFFER_SIZE 64 
+
+volatile extern unsigned char uart0_rx_not_empty_flag;
+volatile extern unsigned char uart0_tx_not_empty_flag;
+volatile extern unsigned char uart0_rx_overf_flag;
+volatile extern unsigned char uart0_tx_overf_flag;
+volatile extern unsigned char uart0_rx_full_flag;
+volatile extern unsigned char uart0_tx_full_flag;
+
+
+//uart0_send_byte is a function to place a byte in Tx_buffer
+void uart0_send_byte(unsigned char value);
+
+//uart0_get_byte is a function to retrieve a byte from Rx_buffer, which is returned
+unsigned char uart0_get_byte(void);
+
+
+#endif
